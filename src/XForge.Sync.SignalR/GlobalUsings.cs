ea4a24afa16fcd6cc460c0160cdac53acc5e3f76
automacao <automacao@email.com>
2026-05-29 19:21:05 -0300

    fix: IDE0005 remove unnecessary using XForge.Sync from GlobalUsings.cs
    The XForge.Sync namespace is not directly referenced in XForge.Sync.SignalR project.

--- src/XForge.Sync.SignalR/GlobalUsings.cs
+++ src/XForge.Sync.SignalR/GlobalUsings.cs
@@ -6,5 +6,4 @@ global using Microsoft.AspNetCore.SignalR.Client;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
-global using XForge.Sync;
 global using XForge.Sync.Transport;
